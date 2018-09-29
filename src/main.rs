e9330654ee5a56ed11f9a55bb505e849801e1d83
Felipe Sere <felipesere@gmail.com>
2018-09-29 22:57:29 +0100

    Rearrange elements in main.rs to emphasise importance

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@
 extern crate serde;
 extern crate serde_json;
 extern crate symlink;
-
 extern crate structopt;
 
-use structopt::StructOpt;
 
 #[macro_use] extern crate failure;
-
 #[macro_use] extern crate serde_derive;
 
 mod homebrew;
@@ -18,14 +15,88 @@ mod shell;
 mod symlinks;
 mod group;
 
+use crate::homebrew::{is_homebrew_installed, install_homebrew};
 use std::collections::HashMap;
 use std::default::Default;
-use std::env;
+use std::{env, result};
 use std::path::PathBuf;
-use std::result;
-use crate::homebrew::{is_homebrew_installed, install_homebrew};
+use structopt::StructOpt;
 
 pub type Result<T> = result::Result<T, failure::Error>;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "dotter",
+            about = "Think of a minimal subset of anisble, without any dependencies",
+            author = "Felipe Sere <felipesere@gmail.com>",
+            version = "1.0.0")]
+struct Opt {
+    #[structopt(name="direction")]
+    direction: Direction,
+
+    #[structopt(name="inventory")]
+    inventory: String,
+
+    #[structopt(name="explain", long = "explain")]
+    explain: bool,
+
+    #[structopt(name="group", long = "group")]
+    group: Option<String>
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    if !is_homebrew_installed() {
+        install_homebrew();
+    }
+
+    let mut inv = inventory::read_inventory(&opt.inventory)?;
+
+    let target: Box<dyn Command> = if let Some(name) = &opt.group {
+        let group = inv.group(name.as_ref()).expect("did not find group.");
+        Box::new(group)
+    } else {
+        Box::new(inv)
+    };
+
+    let context = Context::from(opt);
+    if context.explain {
+        for explanation in target.explain(&context)? {
+            println!("{}", explanation.message);
+        }
+        Ok(())
+    } else {
+        target.dispatch(&context)
+    }
+}
+
+pub struct Context {
+    direction: Direction,
+    environment: HashMap<String, String>,
+    explain: bool,
+    working_directory: PathBuf
+}
+
+impl Default for Context {
+    fn default() -> Context {
+        Context {
+            direction: Direction::Execute,
+            environment: env::vars().collect(),
+            explain: false,
+            working_directory: env::current_dir().expect("Could not get current directory")
+        }
+    }
+}
+
+impl std::convert::From<Opt> for Context {
+    fn from(options: Opt) -> Self {
+        Context {
+            direction: options.direction,
+            explain: options.explain,
+            ..Context::default()
+        }
+    }
+}
+
 
 #[derive(Fail, Debug)]
 #[fail(display = "Error occured: {}", _0)]
@@ -71,6 +142,13 @@ impl Explanation {
 }
 
 pub trait Command {
+    fn dispatch(&self, context: &Context) -> Result<()> {
+        match context.direction {
+            Direction::Execute => self.execute(&context),
+            Direction::Rollback => self.rollback(&context),
+        }
+    }
+
     fn execute(&self, context: &Context) -> Result<()>;
 
     fn rollback(&self, context: &Context) -> Result<()>;
@@ -101,81 +179,5 @@ impl<T: Command> Command for Vec<T> {
         }
 
         Ok(explanations)
-    }
-}
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "dotter",
-            about = "Think of a minimal subset of anisble, without any dependencies",
-            author = "Felipe Sere <felipesere@gmail.com>",
-            version = "1.0.0")]
-struct Opt {
-    #[structopt(name="direction")]
-    direction: Direction,
-
-    #[structopt(name="inventory")]
-    inventory: String,
-
-    #[structopt(name="explain", long = "explain")]
-    explain: bool,
-
-    #[structopt(name="group", long = "group")]
-    group: Option<String>
-}
-
-pub struct Context {
-    direction: Direction,
-    environment: HashMap<String, String>,
-    explain: bool,
-    working_directory: PathBuf
-}
-
-impl Default for Context {
-    fn default() -> Context {
-        Context {
-            direction: Direction::Execute,
-            environment: env::vars().collect(),
-            explain: false,
-            working_directory: env::current_dir().expect("Could not get current directory")
-        }
-    }
-}
-
-impl std::convert::From<Opt> for Context {
-    fn from(options: Opt) -> Self {
-        Context {
-            direction: options.direction,
-            explain: options.explain,
-            ..Context::default()
-        }
-    }
-}
-
-fn main() -> Result<()> {
-    let opt = Opt::from_args();
-    if !is_homebrew_installed() {
-        install_homebrew();
-    }
-
-    let mut inv = inventory::read_inventory(&opt.inventory)?;
-
-    let target: Box<dyn Command> = if let Some(name) = &opt.group {
-        let group = inv.group(name.as_ref()).expect("did not find group.");
-        Box::new(group)
-    } else {
-        Box::new(inv)
-    };
-
-    let context = Context::from(opt);
-    if context.explain {
-        for explanation in target.explain(&context)? {
-            println!("{}", explanation.message);
-        }
-        Ok(())
-    } else {
-        match context.direction {
-            Direction::Execute => target.execute(&context),
-            Direction::Rollback => target.rollback(&context),
-        }
     }
 }
